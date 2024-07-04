404ee6d25f1e9d2547af2a6f88b8d948bd0ff934
Felipe Palazzi <felipepalazzi500@gmail.com>
2024-07-04 17:29:28 -0300

    Cambios para apk

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React from 'react'
 import Main from './src/component/Main.jsx'
 import {NativeRouter} from 'react-router-native'
 import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App () {
   return (
+    <SafeAreaProvider>
     <PaperProvider>
       <NativeRouter>
         <Main />
         </NativeRouter>
       </PaperProvider>
+      </SafeAreaProvider>
   );
 }
 export default App;
