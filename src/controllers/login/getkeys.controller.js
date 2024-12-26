88fe6f38f5fc67ec1553942f0b10d7a5ac1e2200
Felipe Palazzi <felipepalazzi500@gmail.com>
2024-12-26 16:13:34 -0300

    Error corregido en getkeyscontroller

--- src/controllers/login/getkeys.controller.js
+++ src/controllers/login/getkeys.controller.js
@@ -11,8 +11,8 @@ getkeys.conseguirllaves = async (req, res) => {
     }
 
     try {
-        await pool.query(`SELECT k.key_id, k.key_name as "Nombre", k.description as "Descripcion" FROM keys k INNER JOIN user_keys u on u.key_id=k.key_id WHERE u.user_id=$1`, [userId]);
-        res.status(200).send('Acceso eliminado');
+        const result = await pool.query(`SELECT k.key_id, k.key_name as "Nombre", k.description as "Descripcion" FROM keys k INNER JOIN user_keys u on u.key_id=k.key_id WHERE u.user_id=$1`, [userId]);
+        res.status(200).send(result.rows);
     } catch (error) {
         res.status(403).send('Token inválido o expirado');
     }
