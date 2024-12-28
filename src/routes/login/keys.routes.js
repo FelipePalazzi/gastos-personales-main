160a1e499d6f2d3e4765535d38a8b7dd6e100972
Felipe Palazzi <felipepalazzi500@gmail.com>
2024-12-28 09:37:13 -0300

    Me falto importar pagina

--- src/routes/login/keys.routes.js
+++ src/routes/login/keys.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const keysController = require ('../../controllers/login/keys.controller.js');
-const { symbols } = require('../../constants.js');
+const { symbols, pagina} = require('../../constants.js');
 const authenticateToken = require('../../controllers/autenticacion/autenticacion.controller.js');
 
 router.get(`${symbols.barra}${pagina.pagina_key}${symbols.barra}getkeys${symbols.barra}:userId`, authenticateToken, keysController.getkeys );
