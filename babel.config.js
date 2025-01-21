0dc60b6c2f91fb71e7d4faa8d962950528a1fa1e
Felipe Palazzi <felipepalazzi500@gmail.com>
2025-01-21 19:35:50 -0300

    Config babel

--- babel.config.js
+++ babel.config.js
@@ -34,7 +34,35 @@ module.exports = function(api) {
     ],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: [
+          ['module:react-native-dotenv', {
+            'moduleName': '@env',
+            'path': '.env',
+            'blacklist': null,
+            'whitelist': null,
+            'safe': true,
+            'allowUndefined': true
+          }],
+          [
+            '@babel/plugin-transform-class-properties',
+            {
+              'loose': true
+            }
+          ],
+          [
+            '@babel/plugin-transform-private-methods',
+            {
+              'loose': true
+            }
+          ],
+          [
+            '@babel/plugin-transform-private-property-in-object',
+            {
+              'loose': true
+            }
+          ],
+          'react-native-reanimated/plugin'
+        ],
       },
     },
     
